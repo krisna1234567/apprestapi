ac1dcc4621fb9e06ef46106dfdb8b4dda05c10a4
Web Developer Universitas Djuanda <41720264+krisna1234567@users.noreply.github.com>
2021-12-14 16:28:13 +0700

    update controller

--- controller.js
+++ controller.js
@@ -4,6 +4,6 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req, res){
-    response.ok("Aplikasi REST API ku Berjalan")
+    response.ok("Aplikasi REST API ku Berjalan",res)
 };
 
