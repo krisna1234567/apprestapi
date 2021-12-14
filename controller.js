1fca356d910837f62d62cae7a82ca5b68f4ca931
Web Developer Universitas Djuanda <41720264+krisna1234567@users.noreply.github.com>
2021-12-14 16:18:06 +0700

    membuat routes

--- controller.js
+++ controller.js
@@ -6,3 +6,4 @@ var connection = require('./koneksi');
 exports.index = function(req, res){
     response.ok("Aplikasi REST API ku Berjalan")
 };
+
---
+++ routes.js
@@ -0,0 +1,10 @@
+// untuk navigasi
+
+'use strict';
+
+module.exports = function(app){
+    var jsonku = require('./controller');
+
+    app.route('/')
+        .get(jsonku.index);
+}
